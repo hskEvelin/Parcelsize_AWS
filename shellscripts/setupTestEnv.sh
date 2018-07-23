e76a7f39f8029289da2d3ae8f3aa1da2cbf49885
evelin <evelin@evelin-LIFEBOOK-E782>
2018-07-23 15:34:02 +0200

    .

--- shellscripts/setupTestEnv.sh
+++ shellscripts/setupTestEnv.sh
@@ -20,7 +20,7 @@ vagrant ssh -c 'docker network create parcelconfig-net'
 
 #IMPORTANT: start mysql image before microservice
 vagrant ssh -c 'docker run --name mysql-parcelsize --net parcelconfig-net -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=mysqlroot --mount type=bind,src=/vagrant/scripts/,dst=/docker-entrypoint-initdb.d/ mysql:5.7.22'
-sleep 5
+sleep 10s
 
 vagrant ssh -c 'docker run --name webserver --net parcelconfig-net -p 8888:8080 -d parcelwebserver'
 vagrant ssh -c 'docker run --name parcelsize-service_1 --net parcelconfig-net -p 1100:1100 -d parcelsize'
