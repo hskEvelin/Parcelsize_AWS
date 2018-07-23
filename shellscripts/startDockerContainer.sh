ca8a4b9dce2c23a11412ffb514f2098071c7bdc0
evelin <evelin@evelin-LIFEBOOK-E782>
2018-07-23 17:39:43 +0200

    capacity stage

--- shellscripts/startDockerContainer.sh
+++ shellscripts/startDockerContainer.sh
@@ -6,7 +6,7 @@ docker load -i /vagrant/parcelwebserver.tar
 docker load -i /vagrant/mysql.tar
 
 #starten der container
-docker network create parcelconfig-net
+#docker network create parcelconfig-net
 
 docker run --name=mysql-parcelsize --mount type=bind,src=/vagrant/scripts/,dst=/docker-entrypoint-initdb.d/ --net parcelconfig-net -e=MYSQL_ROOT_PASSWORD="mysqlroot" -d mysql/mysql-server:5.7.22
 
@@ -21,6 +21,6 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 for i in $(seq 1 $1)
 do
 	port=$(expr $2 + $i)
-	docker run --name=parcelsize-service_'$i' --net parcelconfig-net -p '$port':1100 -d parcelsize
+	docker run --name=parcelsize-service_$i --net parcelconfig-net -p $port:1100 -d parcelsize
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 done
